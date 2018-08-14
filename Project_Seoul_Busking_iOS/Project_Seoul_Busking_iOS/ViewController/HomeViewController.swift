7d16768c5c498fcbda52aa4beaac3e7cf8d5c183
Unknown <gusgh1492@mail.hongik.ac.kr>
2018-08-15 01:07:25 +0900

    COMMIT // animation tapbar MemberInfo

--- Project_Seoul_Busking_iOS/Project_Seoul_Busking_iOS/ViewController/HomeViewController.swift
+++ Project_Seoul_Busking_iOS/Project_Seoul_Busking_iOS/ViewController/HomeViewController.swift
@@ -6,32 +6,69 @@
 //  Copyright © 2018년 박현호. All rights reserved.
 //
 
+//  현재 회원가입시 -> member_type , member_nickname 저장
+//  현재 로그인시 -> member_ID 저장
 import UIKit
 
 class HomeViewController: UIViewController {
-
-    //  홈들어왔을때 member_Type 저장해야함 어떤식으로 할지 생각해바야함
-    //  현재 회원가입시 -> member_type , member_ID 저장
-    //  현재 로그인시 -> member_ID 저장
-    //  다른 서버 연동하면서 member 에 대한 기본정보를 가져올것인지 생각해바야함
+    
+    @IBOutlet weak var tapbarSearchBtn: UIButton!
+    @IBOutlet weak var tapbarHomeBtn: UIButton!
+    @IBOutlet weak var tapbarMemberInfoBtn: UIButton!
+    @IBOutlet weak var tapbarUIView: UIView!
+    
+    var uiviewX : CGFloat?
+    
+    
     
     @IBOutlet weak var goFirstBtn: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        set()
         setTarget()
+        setTapbarAnimation()
+    }
+    
+    func set() {
+        
+        tapbarUIView.frame.origin.x = uiviewX!
     }
     
     func setTarget() {
         
+        //  로그아웃 버튼
         goFirstBtn.addTarget(self, action: #selector(self.pressedGoFirstBtn(_:)), for: UIControlEvents.touchUpInside)
+        
+        //  검색 버튼
+        tapbarSearchBtn.addTarget(self, action: #selector(self.pressedTapbarSearchBtn(_:)), for: UIControlEvents.touchUpInside)
+        
+        //  개인정보 버튼
+        tapbarMemberInfoBtn.addTarget(self, action: #selector(self.pressedTapbarMemberInfoBtn(_:)), for: UIControlEvents.touchUpInside)
     }
     
+    //  로그아웃 버튼 액션
     @objc func pressedGoFirstBtn( _ sender : UIButton ) {
         
         self.performSegue(withIdentifier: "signin", sender: self)
     }
-
+    
+    //  검색 버튼 액션
+    @objc func pressedTapbarSearchBtn( _ sender : UIButton ) {
+        
+    }
+    
+    //  개인정보 버튼 액션
+    @objc func pressedTapbarMemberInfoBtn( _ sender : UIButton ) {
+        
+        guard let memberInfoVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "MemberInfoViewController") as? MemberInfoViewController else { return }
+        
+        memberInfoVC.uiviewX = self.tapbarHomeBtn.frame.origin.x
+        
+        self.present( memberInfoVC , animated: false , completion: nil )
+        
+    }
+    
     
 }
---
+++ Project_Seoul_Busking_iOS/Project_Seoul_Busking_iOS/ViewController/MemberInfoViewController.swift
@@ -0,0 +1,71 @@
+//
+//  MemberInfoViewController.swift
+//  Project_Seoul_Busking_iOS
+//
+//  Created by 박현호 on 2018. 8. 15..
+//  Copyright © 2018년 박현호. All rights reserved.
+//
+
+import UIKit
+
+class MemberInfoViewController: UIViewController {
+
+    @IBOutlet weak var tapbarSearchBtn: UIButton!
+    @IBOutlet weak var tapbarHomeBtn: UIButton!
+    @IBOutlet weak var tapbarMemberInfoBtn: UIButton!
+    @IBOutlet weak var tapbarUIView: UIView!
+    
+    var uiviewX : CGFloat?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        set()
+        setTarget()
+        setTapbarAnimation()
+    }
+    
+    func set() {
+
+        tapbarUIView.frame.origin.x = uiviewX!
+    }
+    
+    func setTarget() {
+        
+        //  검색 버튼
+        tapbarSearchBtn.addTarget(self, action: #selector(self.pressedTapbarSearchBtn(_:)), for: UIControlEvents.touchUpInside)
+        
+        //  홈 버튼
+        tapbarHomeBtn.addTarget(self, action: #selector(self.pressedTapbarHomeBtn(_:)), for: UIControlEvents.touchUpInside)
+        
+    }
+    
+    func setTapbarAnimation() {
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.01 , execute: {
+
+            UIView.animate(withDuration: 0.75 , delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut , animations: {
+
+                self.tapbarUIView.frame.origin.x = self.tapbarMemberInfoBtn.frame.origin.x
+
+            }, completion: nil )
+        })
+    }
+    
+    //  검색 버튼 액션
+    @objc func pressedTapbarSearchBtn( _ sender : UIButton ) {
+        
+        
+    }
+    
+    //  홈 버튼 액션
+    @objc func pressedTapbarHomeBtn( _ sender : UIButton ) {
+        
+        guard let homeVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "HomeViewController") as? HomeViewController else { return }
+        
+        memberInfoVC.uiviewX = self.tapbarHomeBtn.frame.origin.x
+        
+        self.present( memberInfoVC , animated: false , completion: nil )
+    }
+    
+}
